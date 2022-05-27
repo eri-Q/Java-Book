0b5cf674f5f3984e8ef55ce20560f204d79996c2
EriHatanaka <eri.hatanaka8@gmail.com>
2022-05-27 23:27:53 +0900

    Chapter4復習

---
+++ JavaBook/Chapter4_9_2.java
@@ -0,0 +1,19 @@
+
+public class Chapter4_9_2 {
+	public static void main(String[] args) {
+		int[] moneyList = {191902, 8301, 5510};
+		for (int i : moneyList) {
+			System.out.println(i);
+		}
+		
+		int[] numbers = new int[] {3, 4, 9};
+		System.out.println("１桁の数字を入力してください");
+		int input = new java.util.Scanner(System.in).nextInt();
+		for (int i : numbers) {
+			if (i == input) {
+				System.out.println("アタリ！");			
+			}
+		}
+		
+	}
+}
