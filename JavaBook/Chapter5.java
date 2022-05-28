f239b54f741f5d0f2bb64e47c3d92e5fd8df8602
EriHatanaka <eri.hatanaka8@gmail.com>
2022-05-28 23:57:42 +0900

    Chapter5復習

---
+++ JavaBook/Chapter5.java
@@ -0,0 +1,11 @@
+
+public class Chapter5 {
+	public static void introduceOneself() {
+		String name = "Ema";
+		int age = 30;
+		double height = 164;
+		char zodiac = '戌';
+		System.out.println("私の名前は" + name + "で、" + age + "歳です。");
+		System.out.println("身長は" + height + "cmで、" + zodiac + "年です。");
+	}
+}
