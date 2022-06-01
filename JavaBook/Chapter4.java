8d6dd3054fb103fce37c0a452101dab696bd021a
EriHatanaka <eri.hatanaka8@gmail.com>
2022-06-01 14:49:00 +0900

    Chapter4復習

--- JavaBook/Chapter4.java
+++ JavaBook/Chapter4.java
@@ -21,5 +21,27 @@ public class Chapter4 {
 		System.out.println("合計点: " + sum);
 		System.out.println("平均点: " + avg);
 		System.out.println("50点以上の科目の数は: " + count);
+		
+		int [] seq = new int[10];
+		//塩基配列をランダムに生成
+		for(int i = 0; i < seq.length; i++) {
+			seq[i] = new java.util.Random().nextInt(4);
+		}
+		//生成した塩基配列の記号を表示
+		for(int i = 0; i < seq.length; i++) {
+			switch (seq[i]) {
+			case 0:
+				System.out.print("A ");
+				break;
+			case 1:
+				System.out.print("T ");
+				break;
+			case 2:
+				System.out.print("G ");
+				break;
+			case 3:
+				System.out.print("C ");
+				break;
+			}
 	}
 }
